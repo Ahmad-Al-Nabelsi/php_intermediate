2a1ea46a44210ee88f25baa21ec0a7870dab2fd5
Ahmad-Al-Nabeli <97115953@st.deltion.nl>
2025-03-27 11:08:13 +0100

    UPD css_link_function

--- css_link_function/css_link_function.php
+++ css_link_function/css_link_function.php
@@ -1,12 +1,22 @@
 <?php
 // Functie om alle CSS-bestanden uit de vereiste map op te halen en <link>-tags te maken
+/* Voordelen van deze code
+- Automatisch: U hoeft niet handmatig een <link> aan elk CSS-bestand toe te voegen.
+- Flexibel: werkt met elk aantal CSS-bestanden in de map.
+- Gemakkelijk te onderhouden: Wanneer u een nieuw CSS-bestand toevoegt, wordt dit automatisch 
+geladen zonder dat u de code hoeft te wijzigen.
+
+- Op deze manier kunnen CSS-bestanden eenvoudig worden georganiseerd en worden ze efficiënter geladen!*/
+
 function cssLinks($path) {
     // Controleer of de map bestaat
     if (!is_dir($path)) {
         return "<!-- De map bestaat niet -->";
     }
 
-    // Zoek alle bestanden die eindigen op .css in de map
+    // [De glob()-functie in PHP wordt gebruikt om te zoeken naar bestanden of mappen die voldoen aan een specifiek 
+    //patroon (Pattern Matching) binnen een specifiek pad.
+    //In dit gevaal: Zoek alle bestanden die eindigen op .css in de map]
     $files = glob($path . "/*.css");
 
    // Variabele om de resulterende HTML-code op te slaan
@@ -28,16 +38,18 @@ function cssLinks($path) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>تحميل ملفات CSS تلقائيًا</title>
+    <title>CSS-bestanden automatisch laden</title>
     
     <!-- Roep de functie aan om links naar CSS-bestanden af ​​te drukken -->
-    <?php echo cssLinks("css");?>
+    <?php echo cssLinks("css_bestanden"); ?>
+
 
 </head>
 <body>
 
     <h1> Welkom op mijn website </h1>
-    <p> CSS-bestanden worden automatisch geladen /p>
+    <p> CSS-bestanden worden automatisch geladen </p>
 
 </body>
 </html>
+
--- image_gallery_function.php
+++ image_gallery_function.php
@@ -64,7 +64,6 @@ function showPictures($path) {
             margin: 10px;
             border: 2px solid #ddd;
             border-radius: 10px;
-            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
         }
     </style>
 </head>

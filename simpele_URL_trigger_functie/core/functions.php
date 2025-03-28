c7df778bc364c98006d9ae815a58f75e23cd4b91
Ahmad-Al-Nabeli <97115953@st.deltion.nl>
2025-03-28 09:41:03 +0100

    UPD functions.php

--- simpele_URL_trigger_functie/core/functions.php
+++ simpele_URL_trigger_functie/core/functions.php
@@ -1,4 +1,6 @@
 <?php
+/* Deze functie bevat een invoerreiniger die ongewenste HTML- of PHP-tags 
+verwijdert om de site te beschermen tegen XSS-aanvallen. */
 function sanitize_input($data) {
     return htmlspecialchars(strip_tags($data));
 }
